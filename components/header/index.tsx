bf2de8ef366913890f1de2bdce1d25d597d297b5
LeandroDaSilva <Av@rogue11076>
2021-04-23 13:55:07 -0300

    Adicionado logo na tela de login

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,10 +1,11 @@
 import styles from "./styles.module.scss"
-import {MenuTwoTone} from "@material-ui/icons";
+import {ExitToAppTwoTone, MenuTwoTone} from "@material-ui/icons";
 import {Button} from "react-bootstrap";
 import {toggleMenu} from "../../redux/actions/menuActions";
 import {connect} from "react-redux";
 import {MouseEventHandler} from "react";
 import Link from "next/link"
+import {useRouter} from "next/router";
 
 interface HeaderTypes {
   isOpen: boolean,
@@ -15,6 +16,7 @@ function Header(props: HeaderTypes) {
   const {
     toggleMenu
   } = props;
+  const router = useRouter();
 
   return (
     <header className={styles.container}>
@@ -24,8 +26,13 @@ function Header(props: HeaderTypes) {
         </Button>
         <Link href="/"><p className={styles.logo}>iMachine</p></Link>
       </div>
-      <div>
 
+      <div className={styles.flex}>
+        <Button className={styles.menuButton}
+                variant="outline-light"
+                onClick={event => router.push('/login')}>
+          <ExitToAppTwoTone />
+        </Button>
       </div>
     </header>
   )
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,15 +1,81 @@
-import Head from 'next/head'
-import Menu from "../../components/menu";
-import Header from "../../components/header";
-import Main from "../../components/main";
-import {router} from "next/client";
-import Button from "../../components/button";
+import {useRouter} from "next/router";
+import styles from "./styles.module.scss";
+import {
+  Button,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField
+} from "@material-ui/core";
+import {useState} from "react";
+import {AccountBox, Visibility, VisibilityOff} from "@material-ui/icons";
 
 function Login(props) {
+  const router = useRouter()
+  const [values, setValues] = useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <>
-      <span>Login</span>
-      <Button>Teste</Button>
+      <div className={styles.login}>
+        <div className={styles.background}>
+          <img src="/img/background.png"  alt="logo"/>
+          <div className={styles.logo}>
+            <img src="/img/icon-techplus.svg"  alt="logo"/>
+          </div>
+        </div>
+
+        <div className={styles.loginContainer}>
+          <h1>iMachine</h1>
+          <form method="post">
+            <fieldset>
+              <div>
+                <InputLabel htmlFor="outlined-adornment">Usuário</InputLabel>
+                <OutlinedInput
+                  fullWidth
+                  id="outlined-adornment"
+                  type="text"
+                  value="Lycra"
+                  onChange={handleChange('password')}
+                />
+              </div>
+
+              <div>
+                <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
+                <OutlinedInput
+                  fullWidth
+                  id="outlined-adornment-password"
+                  type={values.showPassword ? 'text' : 'password'}
+                  value={values.password}
+                  onChange={handleChange('password')}
+                />
+              </div>
+            </fieldset>
+
+            <Button style={{backgroundColor: "#272B41", color: "white"}}
+                    onClick={event => router.push("/")}>Logar</Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
