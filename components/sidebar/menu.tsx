2d84fb2580d9b6dc14457fcf7a289962756f6c5e
LeandroDaSilva <Av@rogue11076>
2021-04-26 15:38:52 -0300

    Adicionado outras telas iniciais

--- components/sidebar/menu.tsx
+++ components/sidebar/menu.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 import styles from "./styles.module.scss"
-import {BarChartTwoTone, BusinessTwoTone, SettingsTwoTone, SpeedTwoTone} from "@material-ui/icons";
+import {
+  BarChartTwoTone, Build,
+  BusinessTwoTone,
+  Explore, LocalConvenienceStoreTwoTone,
+  LocationCity,
+  SettingsTwoTone,
+  SpeedTwoTone
+} from "@material-ui/icons";
 
 
 function Menu(props) {
@@ -10,6 +17,10 @@ function Menu(props) {
         <ol className={styles.list}>
           <Link href="/"><li className={styles.item}><SpeedTwoTone /> <span>Dashboard</span></li></Link>
           <Link href="/empresas"><li className={styles.item}><BusinessTwoTone /> <span>Empresas</span></li></Link>
+          <Link href="/unidades"><li className={styles.item}><LocationCity /> <span>Unidades</span></li></Link>
+          <Link href="/areas"><li className={styles.item}><Explore /> <span>Áreas</span></li></Link>
+          <Link href="/equipamentos"><li className={styles.item}><Build /> <span>Equipamentos</span></li></Link>
+          <Link href="/locais"><li className={styles.item}><LocalConvenienceStoreTwoTone /> <span>Locais</span></li></Link>
           <Link href="/sensores"><li className={styles.item}><BarChartTwoTone /> <span>Gráfico dos sensores</span></li></Link>
           <Link href="/configuracoes"><li className={styles.item}><SettingsTwoTone /> <span>Configurações do sistema</span></li></Link>
         </ol>
---
+++ pages/areas/index.tsx
@@ -0,0 +1,69 @@
+import Page from "../../components/page";
+import Layout from "../../components/layout";
+import styled from "styled-components";
+import {useEffect, useState} from "react";
+import CompanyCard from "../../components/companyCard";
+import {Factory} from "../api/factory/list";
+
+const Content = styled.div`
+  height: auto;
+  display: block;
+`
+
+async function loadFactory() {
+  const res = await fetch(`/api/factory/list`)
+  return await res.json();
+}
+
+function Empresas(props) {
+  const [mounted, setMounted] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [factorys, setFactorys] = useState([]);
+
+  const loadingCards = (
+    <>
+      <CompanyCard />
+      <CompanyCard />
+      <CompanyCard />
+    </>
+  )
+
+  useEffect(() => {
+    setMounted(true);
+  })
+
+  useEffect(() => {
+    loadFactory()
+      .then(resp => resp.results[0])
+      .then(data => data.data)
+      .then(fac => fac[0].factorys)
+      .then(fac => setFactorys(fac))
+  }, [mounted])
+
+  useEffect(() => {
+    setIsLoading(false)
+  }, [factorys])
+
+  return (
+    <Layout>
+      <Page title="Áreas">
+        <Content>
+          {
+            isLoading ? loadingCards :
+              <>
+                {factorys.map((row: Factory) =>
+                  <CompanyCard
+                    key={row.factory_id}
+                    factory_image={row.factory_image}
+                    factory_name={row.factory_name}
+                  />
+                )}
+              </>
+          }
+        </Content>
+      </Page>
+    </Layout>
+  )
+}
+
+export default Empresas
