03a91f4fcf135504d61cf6cc9bed1f0ae350c180
LeandroDaSilva <Av@rogue11076>
2021-05-18 14:08:43 -0300

    Refatorado css do sidebar-left

--- components/sidebarLeft/index.tsx
+++ components/sidebarLeft/index.tsx
@@ -1,6 +1,6 @@
 import { closeMenu } from "../../redux/actions/menuActions";
 import { connect } from "react-redux";
-import styled from "styled-components";
+import styles from "./styles.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import {
@@ -9,196 +9,74 @@ import {
   PersonTwoTone,
 } from "@material-ui/icons";
 
-const Container = styled.div`
-  display: flex;
-  position: fixed;
-  left: 0;
-  top: 50px;
-  width: 240px;
-  height: 100%;
-  background-color: #081b2f;
-  z-index: 1;
-  overflow: hidden;
-
-  @media only screen and (max-width: 600px) {
-    width: 0;
-
-    &.block {
-      width: 240px;
-
-      .menu {
-        display: block;
-        left: 0;
-        top: 0;
-        position: absolute;
-      }
-    }
-
-    .open {
-      display: flex;
-      align-items: flex-start;
-      justify-content: center;
-      color: black;
-      width: 280px;
-      max-width: 280px;
-      min-width: 280px;
-      height: 100%;
-      padding: 15px 15px 55px;
-      transition: width ease 0.1s;
-      z-index: 998;
-    }
-
-    .close {
-      .menu {
-        .list {
-          .item {
-            justify-content: center;
-
-            svg {
-              margin-right: 0;
-              width: 100%;
-            }
-
-            span {
-              display: none;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  .children {
-    overflow-y: scroll;
-    width: 100%;
-  }
-
-  .menu {
-    width: 100%;
-
-    .list {
-      list-style: none;
-      width: 100%;
-      padding-left: 0;
-    }
-  }
-`;
-
-const BorderGradient = styled.div`
-  background: ${(props) =>
-    props.active
-      ? "linear-gradient(to right,#4bbef8,#081b2f,#081b2f,#081b2f,#081b2f)"
-      : "#081b2f"};
-  padding: 0px 0px 0px 3px;
-  border-radius: 4px;
-  transition: background ease-out 0.5s;
-
-  &:hover {
-    background: linear-gradient(
-      to right,
-      #08a9fa,
-      #081b2f,
-      #081b2f,
-      #081b2f,
-      #081b2f,
-      #081b2f,
-      #081b2f,
-      #081b2f
-    );
-  }
-`;
-
-const Item = styled.li`
-  background-color: #081b2f;
-  display: flex;
-  align-items: center;
-  -webkit-box-align: center;
-  justify-content: flex-start;
-  width: 100%;
-  border-radius: 4px;
-  font-size: 14px;
-  color: hsla(0, 0%, 100%, 0.65) !important;
-  transition: background-color ease-out 0.5s;
-  padding: 0 30px;
-  color: #343952;
-  height: 40px;
-
-  svg {
-    margin-right: 15px;
-    font-size: 14px;
-  }
-
-  &:hover {
-    cursor: pointer;
-    background-color: rgba(184, 187, 205, 0.1);
-  }
-`;
-
-const Icon = styled.div`
-  margin-right: 5px;
-`;
-
-const Dimmer = styled.div`
-  width: 100vw;
-  height: 100%;
-  display: block;
-  position: fixed;
-  top: 50px;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.35);
-  z-index: 999;
-
-  @media only screen and (min-width: 600px) {
-    display: none;
-  }
-`;
-
 function Sidebar(props) {
   const router = useRouter();
   const { isOpen, children, closeMenu } = props;
 
   return (
     <>
-      <Container className={isOpen ? "block" : ""}>
-        <nav className="menu">
-          <ol className="list">
-            <a href="/">
-              <BorderGradient active={router.pathname === "/"}>
-                <Item className="item" active={router.pathname === "/"}>
-                  <Icon>
+      <div
+        className={
+          isOpen ? `${styles.container} ${styles.block}` : styles.container
+        }
+      >
+        <nav className={styles.menu}>
+          <ol className={styles.list}>
+            <Link href="/">
+              <div
+                className={
+                  router.pathname === "/"
+                    ? `${styles.borderGradient} ${styles.active}`
+                    : styles.borderGradient
+                }
+              >
+                <li className={styles.item}>
+                  <div className={styles.icon}>
                     <HomeTwoTone />
-                  </Icon>{" "}
+                  </div>{" "}
                   <span>Dashboard</span>
-                </Item>
-              </BorderGradient>
-            </a>
+                </li>
+              </div>
+            </Link>
 
-            <a href="/organizacao">
-              <BorderGradient active={router.pathname === "/organizacao"}>
-                <Item className="item">
-                  <Icon>
+            <Link href="/organizacao">
+              <div
+                className={
+                  router.pathname === "/organizacao"
+                    ? `${styles.borderGradient} ${styles.active}`
+                    : styles.borderGradient
+                }
+              >
+                <li className={styles.item}>
+                  <div className={styles.icon}>
                     <BusinessTwoTone />
-                  </Icon>{" "}
+                  </div>{" "}
                   <span>Organização</span>
-                </Item>
-              </BorderGradient>
-            </a>
+                </li>
+              </div>
+            </Link>
 
-            <a href="/usuarios">
-              <BorderGradient active={router.pathname === "/usuarios"}>
-                <Item className="item">
-                  <Icon>
+            <Link href="/usuarios">
+              <div
+                className={
+                  router.pathname === "/usuarios"
+                    ? `${styles.borderGradient} ${styles.active}`
+                    : styles.borderGradient
+                }
+              >
+                <li className={styles.item}>
+                  <div className={styles.icon}>
                     <PersonTwoTone />
-                  </Icon>{" "}
+                  </div>{" "}
                   <span>Usuários</span>
-                </Item>
-              </BorderGradient>
-            </a>
+                </li>
+              </div>
+            </Link>
           </ol>
         </nav>
-      </Container>
-      {isOpen && <Dimmer onClick={closeMenu} />}
-      <div className="children">{children}</div>
+      </div>
+      {isOpen && <div className={styles.dimmer} onClick={closeMenu} />}
+      <div className={styles.children}>{children}</div>
     </>
   );
 }
