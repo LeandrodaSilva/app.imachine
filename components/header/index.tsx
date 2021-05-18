ec7f25b80deda0c22386c8b827367cbe2237a796
LeandroDaSilva <Av@rogue11076>
2021-05-18 14:23:54 -0300

    Refatorado css do header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,11 +2,12 @@ import { MenuTwoTone } from "@material-ui/icons";
 import { Button } from "react-bootstrap";
 import { toggleMenu } from "../../redux/actions/menuActions";
 import { connect } from "react-redux";
-import { MouseEventHandler } from "react";
+import { FC, MouseEventHandler } from "react";
 import Link from "next/link";
 import styled from "styled-components";
 import { User } from "../../types";
 import HeaderUserBox from "../headerUserBox";
+import styles from "./styles.module.scss";
 
 interface HeaderTypes {
   user: User;
@@ -14,113 +15,30 @@ interface HeaderTypes {
   toggleMenu: MouseEventHandler;
 }
 
-const Container = styled.header`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-  background-color: #081b2f;
-  color: white;
-  height: 50px;
-  padding-right: 10px;
-  padding-left: 10px;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.08);
-  z-index: 1;
-
-  @media only screen and (min-width: 600px) {
-    .menuButton {
-      display: none;
-    }
-  }
-
-  .flex {
-    display: flex;
-    padding: 3px;
-  }
-
-  .logo {
-    font-size: 24px;
-    margin-bottom: 0;
-
-    &:hover {
-      cursor: pointer;
-    }
-
-    &:first-letter {
-      color: deepskyblue;
-      font-weight: bold;
-    }
-  }
-
-  .menuButton {
-    border: none;
-    margin-right: 4px;
-    background-color: transparent;
-    color: white;
-
-    &:hover {
-      cursor: pointer;
-      opacity: 0.6;
-    }
-  }
-
-  .userButton {
-    border: none;
-    margin-right: 4px;
-    padding: 4px;
-    background-color: #ffffff;
-    color: #000000;
-    width: 100%;
-    border-radius: 6px;
-
-    &:hover {
-      cursor: pointer;
-      background-color: rgba(117, 118, 124, 0.267);
-      color: #000000;
-    }
-  }
-`;
-
-const NavRight = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-`;
-
-const NavLeft = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-`;
-
-function Header(props: HeaderTypes) {
+const Header: FC<HeaderTypes> = (props) => {
   const { user, toggleMenu } = props;
 
   return (
-    <Container>
-      <NavLeft>
+    <div className={styles.container}>
+      <nav className={styles.navLeft}>
         <Button
-          className="menuButton"
+          className={styles.menuButton}
           variant="outline-light"
           onClick={toggleMenu}
         >
           <MenuTwoTone />
         </Button>
         <Link href="/">
-          <p className="logo">iMachine</p>
+          <p className={styles.logo}>iMachine</p>
         </Link>
-      </NavLeft>
+      </nav>
 
-      <NavRight>
+      <nav className={styles.navRight}>
         <HeaderUserBox />
-      </NavRight>
-    </Container>
+      </nav>
+    </div>
   );
-}
+};
 
 const mapStateToProps = (state) => ({
   user: state.UserObject.user,
